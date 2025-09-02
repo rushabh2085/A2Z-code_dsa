62bba831ec17894a2b8d42ac8e3273952e5b0d99
Rushabh bramhade <rushabh.bramhade123@gmail.com>
2025-09-02 18:23:48 +0530

    Data Structure add Array_example

---
+++ A2Z-code_dsa/Data Structure/Array_example.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Array_example
+{
+	public static void main(String[] args) {
+	
+		int arr[][] = {
+		    {1,2,3},
+		    {4,5,6},
+		    {7,8,9},
+		};
+		
+		for(int i = 0 ; i < 3 ;i++){
+		    for(int j = 0 ; j< 3 ; j++){
+		        System.out.print(arr[i][j]);
+		        
+		    }
+		    System.out.println();
+		}
+		System.out.println(Arrays.deepToString(arr));
+	}
+	
+	
+}
